4c83e7f670069ca2ae07b037a691f24acf66a9d8
Asadcoder786 <anonymousustad798@gmail.com>
2025-11-30 01:43:06 +0500

    Html ,css and javascript Assigmenets

---
+++ app.js
@@ -0,0 +1,21 @@
+let emailInput = document.getElementById("email")
+let passwordInput = document.getElementById("password")
+function submitHandler () {
+    console.log("Email:==>",emailInput.value)
+    console.log("Password:==>",passwordInput.value);
+    
+}
+let firstName = document.getElementById("1stName");
+let lastName = document.getElementById("2ndName")
+let mobileNumber = document.getElementById("mobNo")
+let userPassword = document.getElementById("pass")
+function registerHandler () {
+    console.log("First Name:",firstName.value);
+    console.log("Last Name:",lastName.value);
+    console.log("Mobile Number:",mobileNumber.value);
+    console.log("User Password:",userPassword.value);
+    
+    
+    
+    
+}
